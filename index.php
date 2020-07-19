8c7b58631b082e9e8ab7d07848a73b6bfc5781bc
guy fleury iteriteka <gfleury@disroot.org>
2020-07-19 13:58:27 +0200

    sujet: variable.

--- index.php
+++ index.php
@@ -4,7 +4,25 @@
   <meta charset="utf-8">
   <title>Devoir PHP BASES</title>
 </head>
+<?php
 
+echo '<br> variable <br>';
+$a = 12;
+$b = 12.3;
+$c = true;
+$d = false;
+$e = 'une ligne';
+$f = "une autre ligne";
+
+echo 
+    gettype($a) . ':' . $a . ' '.
+    gettype($b) . ':' . $b . ' '.
+    gettype($c) . ':' . $c . ' '.
+    gettype($d) . ':' . $d . ' '.
+    gettype($e) . ':' . $e . ' '.
+    gettype($f) . ':' . $f . '<br>';
+
+?>
 <body>
 </body>
 </html>
